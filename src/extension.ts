fc079424f1bd6483baa15587007e35d0b64fddfa
Kazuhiro Ebara <ebarakazuhiro@gmail.com>
2024-06-12 11:10:12 +0900

    Support react

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,37 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("catCoding.start", () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        "catCoding",
+        "Cat Coding",
+        vscode.ViewColumn.One,
+        {}
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sample-vscode-extension" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sample-vscode-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sample-vscode-extension!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And set its HTML content
+      panel.webview.html = getWebviewContent();
+    })
+  );
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cat Coding</title>
+</head>
+<body>
+  <div id="app"></div>
+</body>
+</html>`;
+}
