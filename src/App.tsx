4cc66fca1961700091e63144e173b5e4414c5f12
ebarakazuhiro <ebarakazuhiro@gmail.com>
2024-06-12 12:51:06 +0900

    Fixed debug error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 export function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
-      <p>Welcome to your new single-file React.js application!</p>
+      <h1>Reactで動いています。</h1>
+      <img
+        src="https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif"
+        width="300"
+      />
     </div>
   );
 }
